251a71660b248d38cb85ef2222d939dcc48ceb1f
Edgar Felizmenio <edgarfelizmenio@gmail.com>
2018-05-01 17:00:43 +0800

    generate transaction sizes

---
+++ get_transaction_sizes_default.py
@@ -0,0 +1,13 @@
+from pymongo import MongoClient
+import bson
+
+client = MongoClient('localhost', 27017)
+db = client.openhim
+transactions = db.transactions
+txn_list = list(transactions.find())
+txn_sizes = [len(bson.BSON.encode(txn)) for txn in txn_list]
+
+with open("/home/faculty/default_txn_sizes.txt", "w") as txn_sizes_file:
+	txn_sizes_file.writelines([str(txn_size) + "\n" for txn_size in txn_sizes])
+
+
