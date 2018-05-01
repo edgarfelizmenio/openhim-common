b8b69ad6ea38c43108251ccf93db83af06f7550b
Edgar Felizmenio <edgarfelizmenio@gmail.com>
2018-05-01 21:57:45 +0800

    changed txn directory

--- get_transaction_sizes_default.py
+++ get_transaction_sizes_default.py
@@ -7,7 +7,7 @@ transactions = db.transactions
 txn_list = list(transactions.find())
 txn_sizes = [len(bson.BSON.encode(txn)) for txn in txn_list]
 
-with open("/home/faculty/default_txn_sizes.txt", "w") as txn_sizes_file:
+with open("default_txn_sizes.txt", "w") as txn_sizes_file:
 	txn_sizes_file.writelines([str(txn_size) + "\n" for txn_size in txn_sizes])
 
 
